9f1e31920120f971a4e5a15096b8beac84d45fe2
caiges <caigesn@gmail.com>
2015-12-12 17:34:22 -0700

    Make stage the whole width and height of the browser window

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,7 +13,7 @@ WebFontConfig = {
 
 };
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game('100', '100', Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var amish;
 var cursors;
